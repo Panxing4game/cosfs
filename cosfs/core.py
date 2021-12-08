4824181ef51fc07991f385d0a1e5522ed4b028fa
xingxpan <xingxpan@tencent.com>
2021-12-08 15:44:58 +0800

    'ls' feature to test

--- cosfs/core.py
+++ cosfs/core.py
@@ -1,31 +1,57 @@
+import copy
 import logging
 import os
 from os.path import expanduser
 from configparser import ConfigParser
+from typing import Optional, Tuple
+
+import yaml
 from fsspec.asyn import AsyncFileSystem
+from fsspec.spec import AbstractBufferedFile
 from qcloud_cos import CosS3Client, CosConfig
 
 logger = logging.getLogger("cosfs")
 
 
 class COSFileSystem(AsyncFileSystem):
-    def __init__(self, conf_path=expanduser("~")):
-        super().__init__()
-        if os.path.exists(conf_path + "/.cos.conf"):
+    protocol = "cosn"
+
+    def __init__(self, conf_path: Optional[str] = expanduser("~"), secret_id: Optional[str] = None,
+                 secret_key: Optional[str] = None, token: Optional[str] = None, region: Optional[str] = None, **kwargs):
+        super().__init__(**kwargs)
+
+        if secret_id:
+            self.client = CosS3Client(CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token))
+        # coscli config
+        elif os.path.exists(conf_path + "/.cos.yaml"):
+            with open(conf_path + "/.cos.yaml") as f:
+                cli_config = yaml.load(f.read(), Loader=yaml.FullLoader)['cos']
+                if len(cli_config['buckets']) == 0:
+                    raise ValueError("no bucket config found, please check your coscli config file.")
+                self.client = CosS3Client(
+                    CosConfig(Region=cli_config['buckets'][0]['region'], SecretId=cli_config['base']['secretid'],
+                              SecretKey=cli_config['base']['secretkey'], Token=cli_config['base']['sessiontoken']))
+        # coscmd config
+        elif os.path.exists(conf_path + "/.cos.conf"):
             with open(conf_path + "/.cos.conf", 'r') as f:
                 cp = ConfigParser()
                 cp.read_file(f)
                 if not cp.has_section('common'):
-                    raise Exception("[common] section couldn't be found, please check your config file.")
+                    raise ValueError("[common] section couldn't be found, please check your coscmd config file.")
                 secret_id = cp.get('common', 'secret_id', fallback=cp.get('common', 'access_id', fallback=None))
-                self.bucket = cp.get('common', 'bucket')
                 self.client = CosS3Client(CosConfig(Region=cp.get('common', 'region'), SecretId=secret_id,
                                                     SecretKey=cp.get('common', 'secret_key'),
                                                     Token=cp.get('common', 'token', fallback=None)))
-        elif os.path.exists(conf_path + "/.cos.yaml"):
-            raise NotImplementedError("todo")
         else:
             raise FileNotFoundError("No config file found, see: https://cloud.tencent.com/document/product/436/63144")
+
+    def split_path(self, path: str) -> Tuple[str, str]:
+        path = self._strip_protocol(path)
+        path = path.lstrip("/")
+        if "/" not in path:
+            return path, ""
+        bucket_name, obj_name = path.split("/", 1)
+        return bucket_name, obj_name
 
     async def _rm_file(self, path, **kwargs):
         pass
@@ -43,7 +69,34 @@ class COSFileSystem(AsyncFileSystem):
         pass
 
     def ls(self, path, detail=True, **kwargs):
-        pass
+        norm_path = path.strip("/")
+        if norm_path in self.dircache:
+            return copy.deepcopy(self.dircache[norm_path])
+        bucket_name, prefix = self.split_path(path)
+        if bucket_name:
+            list_response = self.client.list_objects(Bucket=bucket_name, Prefix=prefix + "/" if prefix != '' else '',
+                                                     Delimiter="/")
+            info = [{
+                "name": f"{bucket_name}/{obj.get('Key', obj.get('Prefix'))}",
+                "Key": f"{bucket_name}/{obj.get('Key', obj.get('Prefix'))}",
+                "type": "directory" if 'Prefix' in obj or obj['Key'].endswith("/") else "file",
+                "size": 0 if 'Prefix' in obj else obj['Size'],
+                "Size": 0 if 'Prefix' in obj else obj['Size'],
+                "StorageClass": "DIRECTORY" if 'Prefix' in obj or obj['Key'].endswith("/") else "OBJECT",
+                "LastModified": None if 'Prefix' in obj else obj['LastModified']
+            } for obj in list_response.get('Contents', []) + list_response.get('CommonPrefixes', [])]
+        else:
+            info = [{
+                "name": bucket['Name'],
+                "Key": bucket['Name'],
+                "type": "directory",
+                "size": 0,
+                "Size": 0,
+                "StorageClass": "BUCKET",
+                "CreateTime": bucket['CreationDate'],
+            } for bucket in self.client.list_buckets()['Buckets']['Bucket']]
+        self.dircache[norm_path] = info
+        return info
 
     def cp_file(self, path1, path2, **kwargs):
         pass
@@ -58,4 +111,14 @@ class COSFileSystem(AsyncFileSystem):
         pass
 
 
-COSFileSystem()
+class COSFile(AbstractBufferedFile):
+
+    def _fetch_range(self, start, end):
+        pass
+
+
+print(COSFileSystem().ls("cosn://mur-datalake-demo-1255655535/user_upload/weixin_drive/trend_drive/zuopin/zuopin/"))
+print(COSFileSystem().ls("cosn://mur-datalake-demo-1255655535/user_upload/weixin_drive/trend_drive/zuopin/zuopin"))
+print(COSFileSystem().ls("cosn://mur-datalake-demo-1255655535/"))
+print(COSFileSystem().ls("cosn://mur-datalake-demo-1255655535"))
+print(COSFileSystem().ls("cosn://"))
